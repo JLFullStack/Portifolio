dea085a0b1196cc107671d1962941f786b597c75
Lucas Araújo <113523243+JLFullStack@users.noreply.github.com>
2023-12-01 17:28:51 -0300

    implementação de fontes e tema
    implementação das fontes do portifólio e dos temas dark e light

---
+++ assets/lib/themes/themes.js
@@ -0,0 +1,27 @@
+(function () {
+    "use strict";
+
+    const 
+        body = document.body,
+        dark_theme = document.querySelectorAll(".dark"),
+        icon_theme = document.querySelector(".theme-icon");
+
+    const setdarkmode = () => {
+        body.classList.add("dark");
+        body.classList.remove("light");
+        dark_theme.forEach(tag => tag.classList.add("dark"));
+        dark_theme.forEach(tag => tag.classList.remove("light"));
+    }
+
+    const setlightmode = () => {
+        body.classList.add("light");
+        body.classList.remove("dark");
+        dark_theme.forEach(tag => tag.classList.add("light"));
+        dark_theme.forEach(tag => tag.classList.remove("dark"));
+    }
+
+    icon_theme.addEventListener("click", () => {
+        if (body.classList.contains("dark")) setlightmode();
+        else setdarkmode();
+    });
+})();
