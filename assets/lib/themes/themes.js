bf7d3f491d7605d6e79b2282e4526fa095b3a92c
Lucas Araújo <113523243+JLFullStack@users.noreply.github.com>
2023-12-05 17:20:26 -0300

    Temas do Protifólio
    implementação dos temas dark e light

--- assets/lib/themes/themes.js
+++ assets/lib/themes/themes.js
@@ -1,27 +1,32 @@
 (function () {
     "use strict";
 
+    const q = (seletor) => document.querySelector(seletor);
+    const qa = (seletor) => document.querySelectorAll(seletor);
+
     const 
         body = document.body,
-        dark_theme = document.querySelectorAll(".dark"),
-        icon_theme = document.querySelector(".theme-icon");
+        btn_ligth = q("#btn-ligth"),
+        btn_dark = q("#btn-dark"),
+        theme_icon = qa(".theme-icon");
 
-    const setdarkmode = () => {
+    const setDarkMode = () => {
         body.classList.add("dark");
         body.classList.remove("light");
-        dark_theme.forEach(tag => tag.classList.add("dark"));
-        dark_theme.forEach(tag => tag.classList.remove("light"));
+        btn_dark.style = "display:block";
+        btn_ligth.style = "display:none";
     }
 
-    const setlightmode = () => {
+    const setLightMode = () => {
         body.classList.add("light");
         body.classList.remove("dark");
-        dark_theme.forEach(tag => tag.classList.add("light"));
-        dark_theme.forEach(tag => tag.classList.remove("dark"));
+        btn_dark.style = "display:none";
+        btn_ligth.style = "display:block";
     }
 
-    icon_theme.addEventListener("click", () => {
-        if (body.classList.contains("dark")) setlightmode();
-        else setdarkmode();
-    });
+
+    theme_icon.forEach ( icon => 
+        icon.addEventListener("click", () => {
+            body.classList.contains("dark") ? setLightMode() : setDarkMode();
+    }));
 })();
