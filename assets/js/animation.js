b279c66cb11806e5b4e3dc32c16b306f0ae5bcea
Lucas Araújo <113523243+JLFullStack@users.noreply.github.com>
2024-01-03 17:51:08 -0300

    Scroll Buttom
    Implementação do botão de rolagem

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,9 +1,23 @@
+(function () {
+    "use strict";
 
+    const q = (seletor) => document.querySelector(seletor);
+    const qa = (seletor) => document.querySelectorAll(seletor);
 
-// function googleTranslateElementInit() {
-//     new google.translate.TranslateElement({
-//         pageLanguage: 'pt',
-//         // layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
-//     }, 'translate__el');
-// };
+    const setScroll = () => {
+        const btnScroll = q("#btn-scroll");
 
+        window.addEventListener('scroll', () => {
+            window.scrollY > 150 ? btnScroll.classList.add("active") : btnScroll.classList.remove("active");
+        });
+
+        btnScroll.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        });
+    };
+
+    setScroll();
+})();
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,33 @@
     const q = (seletor) => document.querySelector(seletor);
     const qa = (seletor) => document.querySelectorAll(seletor);
 
-    // const setScroll = () => {
-    //     const btnScroll = q("#btn-scroll");
+    const replaceSVGImages = () => {
+        const imgs = qa(".svg");
 
-    //     window.addEventListener('scroll', () => {
-    //         window.pageYOffset > 150 ? btnScroll.classList.add("active") : btnScroll.classList.remove("active");
-    //     });
+        imgs.forEach(img => {
+            const
+                id = img.getAttribute("id"),
+                imgClass = img.getAttribute("class"),
+                url = img.getAttribute("src");
 
-    //     btnScroll.addEventListener('click', () => {
-    //         window.scrollTo({
-    //             top: 0,
-    //             behavior: "smooth",
-    //         });
-    //     });
-    // }
-    // setScroll();
+            fetch(url)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((data) => {
+                    // Create a new div element and set its innerHTML to the SVG content
+                    const icon = document.createElement("i");
+                    icon.innerHTML = data;
 
+                    const svg = icon.querySelector("svg"); // Get the SVG tag from the div                
+                    if (id) svg.setAttribute("id", id); // Add replaced image's ID to the new SVG
+                    if (imgClass) svg.setAttribute("class", imgClass); // Add replaced image's classes to the new SVG                    
+
+                    svg.removeAttribute("xmlns:a"); // Remove any invalid XML tags   
+                    img.parentNode.replaceChild(icon, img); // Replace image with new SVG
+                })
+        });
+    };
+
+    replaceSVGImages();
 })();
